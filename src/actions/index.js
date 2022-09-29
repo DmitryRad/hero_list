cb75eb751f075f616368093510ed3183360bd0c6
DmitryRad <server@internet.ru>
2022-09-29 10:55:39 +0300

    HeroesList on createAsyncThunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,4 @@
-import { heroesFetching, heroesFetched, heroesFetchingError } from "../components/heroesList/heroesSlice";
 import { filtersFetching, filtersFetched, filtersFetchingError } from "../components/heroesFilters/heroesFiltersSlice";
-
-export const fetchHeroes = (request) => (dispatch) => {
-    dispatch(heroesFetching());
-    request("http://localhost:3001/heroes")
-        .then(data => dispatch(heroesFetched(data)))
-        .catch(() => dispatch(heroesFetchingError()))
-}
 
 export const fetchFilters = (request) => (dispatch) => {
     dispatch(filtersFetching());
@@ -15,9 +7,3 @@ export const fetchFilters = (request) => (dispatch) => {
         .catch(() => dispatch(filtersFetchingError()))
 }
 
-export const activeFilterChanged = (filter) => {
-    return {
-            type: 'ACTIVE_FILTER_CHANGED',
-            payload: filter
-        }
-}
